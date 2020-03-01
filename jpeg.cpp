6caf224552421b776e3d4d4816aa0b15d500d088
root <watson@example.com>
2020-03-01 11:05:35 +0800

    fix make code seems easier to understand. 	modified:   jpeg.cpp

--- jpeg.cpp
+++ jpeg.cpp
@@ -2,23 +2,33 @@
 int save_jpeg(AVFrame *pFrame,char *outfile, int width, int height) 
 {
 
-    AVFormatContext *pFormatCtx = avformat_alloc_context();
-    pFormatCtx->oformat = av_guess_format("mjpeg", NULL, NULL);
+	char format[32] = "mjpeg";
+    int got_picture = 0;
+    int pkt_size;
+	int ret;
 
-    if (avio_open(&pFormatCtx->pb, outfile, AVIO_FLAG_READ_WRITE) < 0) {
+    AVPacket pkt;
+    AVFormatContext *pFormatCtx;
+    AVStream *pAVStream;
+    AVCodecContext *pCodecCtx;
+    AVCodec *pCodec;
+
+    pFormatCtx = avformat_alloc_context();
+    pFormatCtx->oformat = av_guess_format(format, NULL, NULL);
+
+    if (avio_open(&pFormatCtx->pb, outfile, AVIO_FLAG_READ_WRITE) < 0) 
         return -1;
-    }
-
-    AVStream *pAVStream = avformat_new_stream(pFormatCtx, 0);
-    if (pAVStream == NULL) {
+    
+	/*Add new stream for encoder*/
+    pAVStream = avformat_new_stream(pFormatCtx, 0);
+    if (pAVStream == NULL) 
         return -1;
-    }
 
-    AVCodecContext *pCodecCtx = pAVStream->codec;
+    pCodecCtx = pAVStream->codec;
 
-    pCodecCtx->codec_id = pFormatCtx->oformat->video_codec;
+    pCodecCtx->codec_id = pFormatCtx->oformat->video_codec;/*Get codec id*/
     pCodecCtx->codec_type = AVMEDIA_TYPE_VIDEO;
-    pCodecCtx->pix_fmt = AV_PIX_FMT_YUVJ420P;
+    pCodecCtx->pix_fmt = AV_PIX_FMT_YUVJ420P;/*The format of AV*/
     pCodecCtx->width = width;
     pCodecCtx->height = height;
     pCodecCtx->time_base.num = 1;
@@ -26,30 +36,28 @@ int save_jpeg(AVFrame *pFrame,char *outfile, int width, int height)
 
     av_dump_format(pFormatCtx, 0, outfile, 1);
 
-    AVCodec *pCodec = avcodec_find_encoder(pCodecCtx->codec_id);
-    if (!pCodec) {
+	/*Get coder*/
+    pCodec = avcodec_find_encoder(pCodecCtx->codec_id);
+    if (!pCodec) 
         return -1;
-    }
-    if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0) {
+    
+    if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0) 
         return -1;
-    }
-
+    
     avformat_write_header(pFormatCtx, NULL);/*write head in file*/
 
-    int y_size = pCodecCtx->width * pCodecCtx->height;
+    pkt_size = pCodecCtx->width * pCodecCtx->height;
 
-    AVPacket pkt;
-    av_new_packet(&pkt, y_size * 3);
+    av_new_packet(&pkt, pkt_size * 3);
 
-    int got_picture = 0;
 	/*from decode to get frame, use frame set in pkt about encode*/
-    int ret = avcodec_encode_video2(pCodecCtx, &pkt, pFrame, &got_picture);/*encode frame*/
-    if (ret < 0) {
+    ret = avcodec_encode_video2(pCodecCtx, &pkt, pFrame, &got_picture);/*encode frame*/
+    if (ret < 0) 
         return -1;
-    }
-    if (got_picture == 1) {
+
+    if (got_picture == 1) 
         ret = av_write_frame(pFormatCtx, &pkt);/*write frame in file*/
-    }
+    
 
     av_free_packet(&pkt);
 
@@ -60,5 +68,6 @@ int save_jpeg(AVFrame *pFrame,char *outfile, int width, int height)
     }
     avio_close(pFormatCtx->pb);
     avformat_free_context(pFormatCtx);
+	avcodec_close(pCodecCtx);
     return 0;
 }
